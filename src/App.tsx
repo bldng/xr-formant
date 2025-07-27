ac3653f336612d6b4cccda4e2cba8146153cb4de
Gerhard Bliedung <gerhard@interactivethings.com>
2025-07-27 21:18:50 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;

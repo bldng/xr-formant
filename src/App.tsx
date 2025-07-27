4a54aacc4dc7928fc120d29c2b4d92584ac60487
Gerhard Bliedung <gerhard@interactivethings.com>
2025-07-27 21:54:14 +0200

    r3f stage

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
+import { Canvas } from "./components/Stage/Canvas";
+
 function App() {
   return (
-    <>
-      <h1>Vite + React</h1>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="w-screen h-screen">
+      <Canvas />
+    </div>
   );
 }
 

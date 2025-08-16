4afb20a052427c530b7568479050ad92d35f34a8
Gerhard Bliedung <gerhard@interactivethings.com>
2025-08-16 17:14:55 +0200

    expose via https

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), basicSsl()],
+  server: {
+    host: true, // Allow external connections for mobile testing
+    port: 5173,
+  },
 });
